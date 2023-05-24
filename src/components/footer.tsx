8ff5c4e3d1b2ddbc484b0290a3a0e037a85e3846
dethMastery <me@suphakit.net>
2023-05-25 03:13:37 +0700

    fix: footer padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
 export const Footer = () => {
   return (
-    <div className="w-full text-center py-8 bg-gradient-to-t from-[black] from-0% to-[transparent] to-120%">
-      <p className="pb-2">
+    <div className="w-full text-center py-6 bg-gradient-to-t from-[black] from-0% to-[transparent] to-120%">
+      <p className="pb-1">
         made w/ 🤍 <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
       </p>
-      <p className="pt-2">
+      <p className="pt-1">
         &copy; 2023 <a href="https://suphakit.net">Suphakit P.</a> All Right Reserved.
       </p>
     </div>
