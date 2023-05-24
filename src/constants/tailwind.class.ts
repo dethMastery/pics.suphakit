eb4b9fe8860702dc322ffcb7c6c00803bd9f082e
dethMastery <me@suphakit.net>
2023-05-25 02:48:44 +0700

    chore: tailwind class const

---
+++ src/constants/tailwind.class.ts
@@ -0,0 +1,14 @@
+export const TAILWIND_CLASS = {
+  BASE: 'bg-pastel-black text-whitesmoke w-full h-auto min-h-screen justify-center text-center',
+  BUTTON:
+    'p-4 rounded-[1rem] w-auto duration-300 hover:scale-85 hover:opacity-60',
+  HEADING: {
+    XXXL: 'text-3xl py-8',
+    XXL: 'text-2xl py-6',
+    XL: 'text-xl py-4',
+    L: 'text-l py-2',
+  },
+  CONTAINER: 'w-[80%] mx-auto text-center',
+  LINK: 'underline duration-300 hover:opacity-60 hover:no-underline',
+  HOVER_SCALE: 'duration-300 hover:scale-85 hover:opacity-60'
+}
