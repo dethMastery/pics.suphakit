bc373f2e1bf579ccd78bb843485437840e237ae1
dethMastery <me@suphakit.net>
2023-05-25 02:55:25 +0700

    chore: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'pastel-black': '#2e2f2f',
+      'whitesmoke': '#f5f5f5',
+      'living-coral': '#fe6e61',
+      'overlay': 'rgba(0,0,0,0.4)',
+    },
+    borderRadius: {
+      DEFAULT: '1rem'
+    },
+    extend: {
+      scale: {
+        '85': '.85'
+      },
+    },
   },
   plugins: [],
 }
