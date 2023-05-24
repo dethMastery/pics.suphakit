07cc202bbea0cf5d998edaff4c48df23ae02a279
dethMastery <me@suphakit.net>
2023-05-25 03:10:45 +0700

    feat: navbar & footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <div className="w-full text-center py-8 bg-gradient-to-t from-[black] from-0% to-[transparent] to-120%">
+      <p className="pb-2">
+        made w/ 🤍 <a href="https://suphakit.net" target="_blank">Suphakit P.</a>
+      </p>
+      <p className="pt-2">
+        &copy; 2023 <a href="https://suphakit.net">Suphakit P.</a> All Right Reserved.
+      </p>
+    </div>
+  )
+}
---
+++ src/components/navBar.tsx
@@ -0,0 +1,16 @@
+const NAVStyle = {
+  BUTTON: 'w-auto inline-block p-4 duration-300 hover:opacity-60 hover:bg-pastel-black'
+}
+
+export const NavBar = () => {
+  return (
+    <div className="w-full flex flex-row bg-[#3e3e3e]">
+      <div className="w-full justify-start text-left">
+        <a href="/" className={NAVStyle.BUTTON}>dethzGallery</a>
+      </div>
+      <div className="w-full justify-end text-right">
+        <a href="/about" className={NAVStyle.BUTTON}>About</a>
+      </div>
+    </div>
+  )
+}
