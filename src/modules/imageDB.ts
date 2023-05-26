11b09ba31f692812e69624f7e95e5d384fca2e51
dethMastery <me@suphakit.net>
2023-05-27 05:00:37 +0700

    chore: image base

--- src/modules/imageDB.ts
+++ src/modules/imageDB.ts
@@ -8,14 +8,14 @@ interface galleryInterface {
 }
 
 export const LINK_BASE = (linkInput: string, imageNo: number) => {
-  return `https://b2.000198.xyz/file/dethzGallery/gallery/${linkInput}/dethz-${imageNo}_marked.jpeg`
+  return `https://ik.imagekit.io/dethzGallery/${linkInput}/dethz-${imageNo}_marked.jpeg`
 }
 
 export const GALLERY: galleryInterface[] = [
   {
     id: 0,
     name: '23/24/05 | Central Sriracha',
-    imageLink: '2023-05-24_CentralSriracha',
+    imageLink: '230524-CentralSriracha',
     pageSlug: '1684886400000',
     gallerySize: 44,
     thumbnailID: 0,
